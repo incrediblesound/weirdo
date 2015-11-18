81df68eeecb3dd4edc685d9c21892622207bf92c
James Edwards <jamese@syapse.com>
2015-11-17 20:44:45 -0800

    add parser tests

---
+++ spec/data/data.js
@@ -0,0 +1,15 @@
+module.exports = {
+	modules: [ '@Calc\n\nInit [..][..]{\n  Self = { \n  deviation: {num: 0, avg: 0, sum: 0, log: [], dev: 0 },\n  max: 0\n  };\n}\n\n.max [Num][~]{\n\tx = Self > n ? Self : n;\n\tSelf = x;\n}\n\n.deviation [..][->]{\n\t<- Self.dev;\n}\n\n.deviation [Num][~]{\n\tSelf.num++;\n\tSelf.sum += n;\n\tSelf.log.push(n);\n\tdiffs = [];\n\tx = Self.sum / Self.num;\n\teach Self.log y ->\n\t\tz = y - x;\n\t\tz = z * z;\n\t\tdiffs.push(z);\n\teach;\n\n\ta = 0;\n\teach diffs b ->\n\t\ta += b;\n\teach;\n\tc = a / diffs.length;\n\n\tSelf.dev = Math.sqrt(c);\n}',
+  '@Person\n\nInit [Str][name]{\n\tSelf = {info: { name: name, age: null }}\n}\n\n.info [..][->]{\n\tx = Self.name + ", "+ Self.age + " yo."; \n\t<- x;\n}\n\n.info [Num, Str][age, name]{\n\tSelf.age = age;\n\tSelf.name = name;\n}\n\n@Another\n\nInit [..][..]{\n\tSelf = 0;\t\n}\n\n.info [..][n]{\n\tSelf = n\n}\n' ],
+    main: [ '', 'calc <- Calc', 'person <- Person[ "John" ]', '', 'sum <- Num', 'dev <- Num', 'info <- Str', 'name <- Str', '', 'calc.max[ 2 , 3 ]', 'calc.max[ 13 ]', 'calc.max[ 7 ]', 'calc.deviation[ 13,24,56,81 ]', 'calc.deviation -> dev', 'sys.out[ dev ]', 'calc.deviation[ 42 ]', 'person.info[ 13, "John Doe" ]', '', 'calc.max -> sum', 'calc.deviation -> dev', 'person.info -> info', '', 'sys.out[ dev  ]', 'sys.out[ info ]' ],
+    init: ['person <- Person[ "John" ]'],
+    instance: ['string <- Str'],
+    invocation: ['calc.max[ 7 ]'],
+    modulePieces: {
+	    declaration: '@Calc\n\n',
+	    initialize: 'Init [..][..]{\n  Self = { \n  deviation: {num: 0, avg: 0, sum: 0, log: [], dev: 0 },\n  max: 0\n  };\n}\n\n',
+	    method_one: '.max [Num][~]{\n\tx = Self > n ? Self : n;\n\tSelf = x;\n}\n\n',
+	    getter: '.deviation [..][->]{\n\t<- Self.dev;\n}\n\n',
+	    method_two:'.deviation [Num][~]{\n\tSelf.num++;\n\tSelf.sum += n;\n\tSelf.log.push(n);\n\tdiffs = [];\n\tx = Self.sum / Self.num;\n\teach Self.log y ->\n\t\tz = y - x;\n\t\tz = z * z;\n\t\tdiffs.push(z);\n\teach;\n\n\ta = 0;\n\teach diffs b ->\n\t\ta += b;\n\teach;\n\tc = a / diffs.length;\n\n\tSelf.dev = Math.sqrt(c);\n}'	
+    }
+}
--- spec/parserTests.js
+++ spec/parserTests.js
@@ -1,14 +1,83 @@
 var ParseText = require('../lib/utility/parseText.js');
 var parsers = require('../lib/parsers.js');
+var data = require('./data/data.js');
 
 describe("Main Parser", function(){
 	it("parses instances", function(){
-		var lines = ['string <- Str\n'];
+		var lines = data.instance;
 		var result = parsers.mainParser(lines);
 		result = result[0];
 		expect(result).not.toBe(undefined);
 		expect(result.type).toBe('instance');
 		expect(result.data[0].type).toBe('reference');
 		expect(result.data[1].type).toBe('reference');
+	})
+	it("parses invocations", function(){
+		var lines = data.invocation;
+		var result = parsers.mainParser(lines);
+		result = result[0];
+		expect(result).not.toBe(undefined);
+		expect(result.type).toBe('invocation');
+		expect(result.data.object).toBe('calc');
+		expect(result.data.method).toBe('max');
+		expect(result.data.args).toEqual(['7']);
+	})
+	it("parses initialization", function(){
+		var lines = data.init;
+		var result = parsers.mainParser(lines);
+		result = result[0];
+		var initialization = result.data[1];
+		expect(initialization).not.toBe(undefined);
+		expect(initialization.type).toBe('initialize');
+		expect(initialization.data.object).toBe('Person');
+		expect(initialization.data.args).toEqual(['"John"']);
+	})
+})
+
+describe("Module Parser", function(){
+	it("parses declarations", function(){
+		var parseText = new ParseText(data.modulePieces.declaration);
+		var result = parsers.moduleParser(parseText);
+		expect(result[0].type).toBe('object_declaration');
+		expect(result[0].data).toBe('Calc');
+	})
+	it("parses initialization", function(){
+		var parseText = new ParseText(data.modulePieces.initialize);
+		var result = parsers.moduleParser(parseText);
+		expect(result[0].type).toBe('object_init');
+		expect(result[0].data[0].type).toBe('method_types');
+		expect(result[0].data[0].data).toEqual(['..']);
+		expect(result[0].data[1].type).toBe('method_args');
+		expect(result[0].data[1].data).toEqual(['..']);
+		expect(result[0].data[2].type).toBe('method_body');
+		expect(result[0].data[2].data[0]).toMatch(/.*Self\s=\s\{/);
+		// expect(result[0].data).toBe('Calc');
+	})
+	it("parses methods", function(){
+		var parseText = new ParseText(data.modulePieces.method_one);
+		var result = parsers.moduleParser(parseText);
+		expect(result[0].type).toBe('object_method');
+		expect(result[0].data[0].type).toBe('method_name');
+		expect(result[0].data[0].data).toBe('max');
+		expect(result[0].data[1].type).toBe('method_types');
+		expect(result[0].data[1].data).toEqual(['Num']);
+		expect(result[0].data[2].type).toBe('method_args');
+		expect(result[0].data[2].data).toEqual(['~']);
+		expect(result[0].data[3].type).toBe('method_body');
+		expect(result[0].data[3].data.length).toBe(3);
+	})
+	it("parses getters", function(){
+		var parseText = new ParseText(data.modulePieces.getter);
+		var result = parsers.moduleParser(parseText);
+		console.log(result[0]);
+		expect(result[0].type).toBe('object_method');
+		expect(result[0].data[0].type).toBe('method_name');
+		expect(result[0].data[0].data).toBe('deviation');
+		expect(result[0].data[1].type).toBe('method_types');
+		expect(result[0].data[1].data).toEqual(['..']);
+		expect(result[0].data[2].type).toBe('method_args');
+		expect(result[0].data[2].data).toEqual(['->']);
+		expect(result[0].data[3].type).toBe('method_body');
+		expect(result[0].data[3].data.length).toBe(2);
 	})
 })
