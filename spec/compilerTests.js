ab20039f9f4dc43415ee10673fd4551fa425cdb9
James Edwards <jamese@syapse.com>
2015-11-20 22:08:10 -0800

    add transpiler tests

--- spec/compilerTests.js
+++ spec/compilerTests.js
@@ -6,9 +6,17 @@ describe("Compile Modules", function(){
 	it("transpiles module methods", function(){
 		var control = new Control();
 		var declaration = data.parsedPieces.objectDeclaration;
+
 		compiler(declaration, control);
-		console.log(control);
+
+		expect(control.objects["Calc"]).toEqual({ getters: {} });
+		expect(control.footer).toBe('wdo_modules.Calc = Calc;\n');
+
 		var method = data.parsedPieces.objectMethod;
 		compiler(method, control);
+
+		expect(control.objects.Calc.max).toBeDefined();
+		expect(control.objects.Calc.max).toEqual({ args: '~', variables: { x:true }, argTypes: 'Num' })
+		expect(control.result).toBe('Calc.prototype.max = function(){\nvar args = Array.prototype.slice.call(arguments);\nvar method_body = function(n){\nvar x = this.data["max"] > n ? this.data["max"] : n;\nthis.data["max"] = x;\n}\nwdo.invokeRecursive(method_body, this, args);\nx = this.data["max"] > n ? this.data["max"] : n;\nthis.data["max"] = x;\n}\n');
 	})
 })
